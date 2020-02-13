2c78745f84658b85e321d4771c06c78cd7895e65
Ramakrishna Bodi <brkrishna@gmail.com>
2020-02-13 17:31:22 +0530

    initial commit

---
+++ dashboard/boards/views.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
+from django.http import HttpResponse
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards':boards})
+
+def about(request):
+    return render(request, 'about.html')
+
+def about_company(request):
+    return render(request, 'about_company.html', {'company_name':'Simple Complex'})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board':board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board':board,'form':form})
